bc51a72dc2ab653fbbc3ace03ca5e856a182ab6f
William Huang <huang.william.cn@hotmail.com>
2017-01-29 14:28:27 -0500

    Adds more criteria in joystick trigger. Now the trigger is enabled only when in teleop/operator control mode and the joysticks are left alone.

--- src/org/usfirst/frc/team1391/robot/triggers/JoystickTrigger.java
+++ src/org/usfirst/frc/team1391/robot/triggers/JoystickTrigger.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team1391.robot.triggers;
 
+import org.usfirst.frc.team1391.robot.OI;
+
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.buttons.Trigger;
 
 /**
@@ -9,10 +12,15 @@ import edu.wpi.first.wpilibj.buttons.Trigger;
 public class JoystickTrigger extends Trigger {
 
     public boolean get() {
-    	if(DriverStation.getInstance().isOperatorControl()){
-        return true;
-    	}else{
-    	return false;
+    	
+    	double driverX = OI.driver.getAxis(AxisType.kX);
+    	double driverY = OI.driver.getAxis(AxisType.kY);
+    	double driverZ = OI.driver.getAxis(AxisType.kZ);
+    	double driverThrottle = OI.driver.getAxis(AxisType.kThrottle);
+    	if(DriverStation.getInstance().isOperatorControl() && driverX == 0 && driverY == 0 && driverZ == 0 && driverThrottle == 0){
+    		return true;
     	}
+    	
+    	return false;
     }
 }
